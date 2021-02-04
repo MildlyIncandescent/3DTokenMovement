a7340e6547d57a2d166a66e0fe6f0c91cda26b0f
MildlyIncandescent <15236243+MildlyIncandescent@users.noreply.github.com>
2021-02-03 18:11:48 -0800

    Update 3dMovement.js

--- 3dMovement.js
+++ 3dMovement.js
@@ -1,3 +1,24 @@
-function translateTokenPosition(token) {
+const T3D {
+	SCOPE: "3d-token-movement",
+	SETTING_AREA_WIDTH: "area width",
+	LOG_PREFIX: "3D Token Movement | " 
+};
+
+class Token3D {
 	
+	constructor () {
+		game.settings.register(T3D.SCOPE, T3D.SETTING_AREA_WIDTH, { 
+			name: "3D Area Width",
+			hint: "",
+			config: true,
+			type: Integer,
+			default: 1200
+		});
+		Hooks.on("updateToken", this._onUpdateToken.bind(this));
+		console.log(T3D.LOG_PREFIX, "Initialized");
+	}
+	
+	_onTokenUpdate() {
+		
+	}
 }
